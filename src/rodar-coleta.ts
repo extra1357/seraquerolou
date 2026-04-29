61fa08e9dd0064b912baffa5b43ca55c6d7800ad
extra1357 <salvacao.cristo@gmail.com>
2026-04-29 07:11:55 -0300

    feat: coleta via GitHub Actions a cada hora + remove botão

---
+++ src/rodar-coleta.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { executarColeta } from './coletor';
+executarColeta().then(() => {
+  console.log('Coleta finalizada!');
+  process.exit(0);
+}).catch((err) => {
+  console.error('Erro na coleta:', err);
+  process.exit(1);
+});
